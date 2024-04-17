05728799c89abdcef84c762c04e056e512b1a3f2
nikhita kurdikeri <131981430+kurdikerinick@users.noreply.github.com>
2024-04-17 15:21:22 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { getDatabase } from 'firebase/database';
 import { initializeApp } from 'firebase/app';
 import 'firebase/auth';
 const firebaseConfig = {
-  apiKey: "AIzaSyAaaxRXLSTUCNRW3sjgzMn5ocwh-KEdMZE",
-  authDomain: "srtdemo-dfee0.firebaseapp.com",
-  databaseURL: "https://srtdemo-dfee0-default-rtdb.firebaseio.com",
-  projectId: "srtdemo-dfee0",
-  storageBucket: "srtdemo-dfee0.appspot.com",
-  messagingSenderId: "488445210099",
-  appId: "1:488445210099:web:a094318e1cdd2ef37a7686"
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxX",
+  authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 };
 
 // Initialize Firebase
